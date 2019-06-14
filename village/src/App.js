e4a6c1c9b5f8f40c58e3c54c29cb4849b2b2d6c5
MMangus <mjmangus16@yahoo.com>
2019-06-14 11:40:56 -0400

    added DELETE function to remove a smurf

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
+import Nav from "./components/Nav";
 
 import axios from "axios";
 import { Route } from "react-router-dom";
@@ -25,6 +26,13 @@ class App extends Component {
   addSmurf = smurf => {
     axios.post("http://localhost:3333/smurfs", smurf).then(res => {
       this.setState({ smurfs: res.data });
+      this.props.history.push("/");
+    });
+  };
+
+  deleteSmurf = id => {
+    axios.delete(`http://localhost:3333/smurfs/${id}`).then(res => {
+      this.setState({ smurfs: res.data });
     });
   };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -33,8 +41,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Nav />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Smurfs smurfs={this.state.smurfs} deleteSmurf={this.deleteSmurf} />
+          )}
+        />
+        <Route
+          exact
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} addSmurf={this.addSmurf} />}
+        />
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
+    <div
+      className="Smurf"
+      style={{ border: "1px solid black", marginTop: 20, padding: "25px 0px" }}
+    >
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button onClick={() => props.deleteSmurf(props.id)}>DELETE SMURF</button>
     </div>
   );
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
